21e6b6d16d4e1a3c2f0d7a6443ceda33d09d5b69
Philipp Rohde <Philipp.Rohde@tib.eu>
2021-11-24 09:11:23 +0100

    on configure only extract molecules if necessary

--- app/query.py
+++ app/query.py
@@ -1,17 +1,13 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, session, Response, send_from_directory, request, url_for
+    Blueprint, request
 )
-from werkzeug.exceptions import abort
 import os
 from flask.json import jsonify
-from time import time
 import traceback
-from multiprocessing import Process, Queue, active_children
-import hashlib
 import json
 import logging
 
-from awudima import AwudimaFQP, Federation, DataSourceType
+from awudima import AwudimaFQP, Federation
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -38,32 +34,29 @@ def sparql():
             if os.path.exists(configfile):
                 federation = Federation.config(configfile)
             else:
-                return jsonify({
-                                    "head": {
-                                        "vars": []
-                                    },
-                                    "results": {
-                                        "bindings": []
-                                    },
-                                    'message': 'Error in executing the query!',
-                                    'query': query,
-                                    "error": "Federation setting is not found as '/data/federation.json'"})
+                return jsonify({"head": {
+                                    "vars": []
+                                },
+                                "results": {
+                                    "bindings": []
+                                },
+                                "message": "Error in executing the query!",
+                                "query": query,
+                                "error": "Federation setting is not found as '/data/federation.json'"})
             fqp = AwudimaFQP(federation)
             resultset = fqp.execute(query, keep_in_memory=True)
             if resultset:
                 return jsonify(resultset.results)
             else:
-                return jsonify({
-                                    "head": {
-                                        "vars": []
-                                    },
-                                    "results": {
-                                        "bindings": []
-                                    },
-                                    'message': 'Error in executing the query!',
-                                    'query': query,
-                                    'error': 'Exception while executing the query!'
-                                })
+                return jsonify({"head": {
+                                    "vars": []
+                                },
+                                "results": {
+                                    "bindings": []
+                                },
+                                "message": "Error in executing the query!",
+                                "query": query,
+                                "error": "Exception while executing the query!"})
         except Exception as e:
             import sys
             exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -72,59 +65,53 @@ def sparql():
             print("Exception: ", e)
             import pprint
             pprint.pprint(emsg)
-            return jsonify({
-                                    "head": {
-                                        "vars": []
-                                    },
-                                    "results": {
-                                        "bindings": []
-                                    },
-                                    'message': 'Error in executing the query!',
-                                    'query': request.args.get("query", 'No SPARQL query found'),
-                                    "error": str(emsg)
-                    })
+            return jsonify({"head": {
+                                "vars": []
+                            },
+                            "results": {
+                                "bindings": []
+                            },
+                            "message": "Error in executing the query!",
+                            "query": request.args.get("query", "No SPARQL query found"),
+                            "error": str(emsg)})
 
     else:
-        return jsonify({
-                        "head": {
+        return jsonify({"head": {
                             "vars": []
                         },
                         "results": {
                             "bindings": []
                         },
-                        'message': 'Error in executing the query!',
-                        'query': 'No SPARQL query found',
-                        "error": "Invalid HTTP method used. Use GET "}
-                )
+                        "message": "Error in executing the query!",
+                        "query": "No SPARQL query found",
+                        "error": "Invalid HTTP method used. Use GET "})
 
 
 @bp.route("/configure", methods=['POST', 'GET'])
 def configure():
     if request.method == 'GET' or request.method == 'POST':
         try:
-            conf = request.args.get("federation", {})
+            conf = request.args.get("federation", {}) if request.method == 'GET' else request.values.get("federation", {})
             fed = json.loads(conf)
             federation = Federation.config(fed)
-            federation.extract_molecules()
+            if not federation.rdfmts:
+                logger.info("No molecules in config, extracting them now!")
+                federation.extract_molecules()
             federation.dump_to_json(configfile)
             return jsonify({'status': True, 'federation': federation.to_json()})
         except Exception as e:
             import sys
             exc_type, exc_value, exc_traceback = sys.exc_info()
-            emsg = repr(traceback.format_exception(exc_type, exc_value,
-                                                   exc_traceback))
+            emsg = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
             logger.error("Exception  " + emsg)
             print("Exception: ", e)
             import pprint
             pprint.pprint(emsg)
-            return jsonify({'status': False,
-                            "error": str(emsg)
-                            })
-
+            return jsonify({"status": False,
+                            "error": str(emsg)})
     else:
-        return jsonify({'status': False,
-                        "error": "Invalid HTTP request! Only GET and POST are supported!"
-                            })
+        return jsonify({"status": False,
+                        "error": "Invalid HTTP request! Only GET and POST are supported!"})
 
 
 @bp.route("/inspect", methods=['POST', 'GET'])
@@ -138,16 +125,13 @@ def inspect():
         except Exception as e:
             import sys
             exc_type, exc_value, exc_traceback = sys.exc_info()
-            emsg = repr(traceback.format_exception(exc_type, exc_value,
-                                                   exc_traceback))
+            emsg = repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
             logger.error("Exception  " + emsg)
             print("Exception: ", e)
             import pprint
             pprint.pprint(emsg)
             return jsonify({"federation": None,
-                            "error": str(emsg)
-                            })
+                            "error": str(emsg)})
     else:
         return jsonify({"federation": None,
-                        "error": "Invalid HTTP request! Only GET and POST are supported!"
-                        })
+                        "error": "Invalid HTTP request! Only GET and POST are supported!"})
